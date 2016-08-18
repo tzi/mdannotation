a8f3da08fb9bc14d5a297b8e7e0164af333a1383
Thomas ZILLIOX <thomas@zilliox.me>
2016-08-18 18:24:11 +0200

    Allow to use several plugins in generic converter

--- packages/mdannotation/src/converter.js
+++ packages/mdannotation/src/converter.js
@@ -2,10 +2,11 @@ const markdown = require('markdown').markdown;
 const posthtml = require('posthtml');
 
 module.exports = function converter(plugins) {
+    const pluginList = Array.isArray(plugins) ? plugins : [plugins];
+    
     return function(md) {
         const html = markdown.toHTML(md);
-        return posthtml()
-            .use(plugins)
+        return posthtml(pluginList)
             .process(html)
         ;
     }
